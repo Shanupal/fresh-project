9a2b4caf679d406951abf2351e63e261b8b8b4e9
Shanupal <164392576+Shanupal@users.noreply.github.com>
2024-11-25 18:27:20 +0530

    Add files via upload

---
+++ assest/js/buttons.js
@@ -0,0 +1,10 @@
+let abtBtn = document.getElementById("aboutPage");
+let btnAbt = document.getElementById("abtPage");
+
+abtBtn.addEventListener("click", ()=>{
+    window.location.href = "about.html";
+})
+
+btnAbt.addEventListener("click", ()=>{
+    window.location.href = "about.html";
+})
